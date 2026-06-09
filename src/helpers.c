939a2b2aee6790a4704746968cd568a5e6cd711d
Fedor Skrebnev <skrebnev30@gmail.com>
2026-06-09 19:28:28 +0200

    Add README with project description and usage instructions

--- src/helpers.c
+++ src/helpers.c
@@ -3,80 +3,80 @@
 #include <stdio.h>
 
 DisplayServer detect_display_server(void) {
-  const char *session_type = getenv("XDG_SESSION_TYPE");
+    const char *session_type = getenv("XDG_SESSION_TYPE");
 
-  if (session_type != NULL) {
-    if (strcmp(session_type, "wayland") == 0) return DISPLAY_WAYLAND;
-    if (strcmp(session_type, "x11") == 0) return DISPLAY_X11;
-  }
+    if (session_type != NULL) {
+        if (strcmp(session_type, "wayland") == 0)
+            return DISPLAY_WAYLAND;
+        if (strcmp(session_type, "x11") == 0)
+            return DISPLAY_X11;
+    }
 
-  if (getenv("WAYLAND_DISPLAY") != NULL) {
-    return DISPLAY_WAYLAND;
-  }
+    if (getenv("WAYLAND_DISPLAY") != NULL) {
+        return DISPLAY_WAYLAND;
+    }
 
-  if (getenv("DISPLAY") != NULL) {
-    return DISPLAY_X11;
-  }
+    if (getenv("DISPLAY") != NULL) {
+        return DISPLAY_X11;
+    }
 
-  return DISPLAY_UNKNOWN;
+    return DISPLAY_UNKNOWN;
 }
 
 int copy_to_clipboard(DisplayServer env, char const *password) {
- #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__)
-  (void)env;
-  FILE *pipe = popen("pbcopy", "w");
-  if(!pipe) {
-    perror("clipboard (pbcopy)");
-    return STATUS_CANNOT_COPY_TO_CLIPBOARD;
-  }
-
-  fprintf(pipe, "%s", password);
-  
-  int status = pclose(pipe);
-  if (status != 0) {
-    fprintf(stderr, "Warning: clipboard copy may have failed (exit %d)\n", status);
-  } else {
-    fprintf(stderr, "Password copy to clipboard\n");
-  }
-  return STATUS_OK;
- #else
-  if (env == DISPLAY_UNKNOWN) {
-    fprintf(stderr, "Cannot detect display server. Password was not copied to clipboard\n");
-    return STATUS_CANNOT_DETECT_DISPLAY;
-  }
-
-  if (env == DISPLAY_WAYLAND) {
-    FILE *pipe = popen("wl-copy", "w");
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__)
+    (void)env;
+    FILE *pipe = popen("pbcopy", "w");
     if (!pipe) {
-      perror("clipboard (wl-copy)");
-      return STATUS_CANNOT_COPY_TO_CLIPBOARD;
+        perror("clipboard (pbcopy)");
+        return STATUS_CANNOT_COPY_TO_CLIPBOARD;
     }
 
     fprintf(pipe, "%s", password);
 
     int status = pclose(pipe);
     if (status != 0) {
-      fprintf(stderr, "Warning: clipboard copy may have failed (exit %d)\n", status);
+        fprintf(stderr, "Warning: clipboard copy may have failed (exit %d)\n", status);
     } else {
-      fprintf(stderr, "Password copied to clipboard\n");
+        fprintf(stderr, "Password copy to clipboard\n");
     }
-  } else {
-    FILE *pipe = popen("xclip -selection clipboard", "w");
-    if (!pipe) {
-      perror("clipboard (xclip)");
-      return STATUS_CANNOT_COPY_TO_CLIPBOARD;
+    return STATUS_OK;
+#else
+    if (env == DISPLAY_UNKNOWN) {
+        fprintf(stderr, "Cannot detect display server. Password was not copied to clipboard\n");
+        return STATUS_CANNOT_DETECT_DISPLAY;
     }
 
-    fprintf(pipe, "%s", password);
+    if (env == DISPLAY_WAYLAND) {
+        FILE *pipe = popen("wl-copy", "w");
+        if (!pipe) {
+            perror("clipboard (wl-copy)");
+            return STATUS_CANNOT_COPY_TO_CLIPBOARD;
+        }
 
-    int status = pclose(pipe);
-    if (status != 0) {
-      fprintf(stderr, "Warning: clipboard copy may have failed (exit %d)\n", status);
+        fprintf(pipe, "%s", password);
+
+        int status = pclose(pipe);
+        if (status != 0) {
+            fprintf(stderr, "Warning: clipboard copy may have failed (exit %d)\n", status);
+        } else {
+            fprintf(stderr, "Password copied to clipboard\n");
+        }
     } else {
-      fprintf(stderr, "Password copied to clipboard\n");
+        FILE *pipe = popen("xclip -selection clipboard", "w");
+        if (!pipe) {
+            perror("clipboard (xclip)");
+            return STATUS_CANNOT_COPY_TO_CLIPBOARD;
+        }
+
+        fprintf(pipe, "%s", password);
+
+        int status = pclose(pipe);
+        if (status != 0) {
+            fprintf(stderr, "Warning: clipboard copy may have failed (exit %d)\n", status);
+        }
     }
-  }
-  
-  return STATUS_OK;
+
+    return STATUS_OK;
 #endif
-} 
+}
--- src/main.c
+++ src/main.c
@@ -12,87 +12,90 @@ const int MAX_OPTIONS = 3;
 const int MIN_OPTIONS = 1;
 
 int main(int argc, char **argv) {
-  if (argc == MIN_OPTIONS || argc > MAX_OPTIONS) {
-    fprintf(stderr, "Usage: -g <len>, -h for help\n");
-    exit(STATUS_USAGE_ERROR);
-  }
+    if (argc == MIN_OPTIONS || argc > MAX_OPTIONS) {
+        fprintf(stderr, "Usage: -g <len>, -h for help\n");
+        exit(STATUS_USAGE_ERROR);
+    }
 
-  DisplayServer display_server = detect_display_server();
-    
-  int opt;
-  while ((opt = getopt(argc, argv, "hg:")) != -1) {
-    switch(opt) {
-      case 'g': {
-        char *endptr;
-        errno = 0;
-        
-        long arg = strtol(optarg, &endptr, 10);
+    DisplayServer display_server = detect_display_server();
 
-        if (errno == ERANGE) {
-          if (arg == LONG_MAX) {
-            fprintf(stderr, "Password length exceeds the maximum of %d\n", MAX_PASSWORD_LENGTH);
-            exit(STATUS_PASSWORD_LENGTH_ERROR);
-          }
-          if (arg == LONG_MIN) {
-            fprintf(stderr, "Password length is below the minimum of %d\n", MIN_PASSWORD_LENGTH);
-            exit(STATUS_PASSWORD_LENGTH_ERROR);
-          }
-        }
+    int opt;
+    while ((opt = getopt(argc, argv, "hg:")) != -1) {
+        switch (opt) {
+            case 'g': {
+                char *endptr;
+                errno = 0;
 
-        if (*endptr != '\0') {
-          fprintf(stderr, "Trailing characters after the number\n");
-          exit(STATUS_NOT_A_NUMBER);
-        }
+                long arg = strtol(optarg, &endptr, 10);
 
-        if (optarg == endptr) {
-          fprintf(stderr, "Not a valid number\n");
-          exit(STATUS_NOT_A_NUMBER);
-        }
+                if (errno == ERANGE) {
+                    if (arg == LONG_MAX) {
+                        fprintf(stderr, "Password length exceeds the maximum of %d\n",
+                                MAX_PASSWORD_LENGTH);
+                        exit(STATUS_PASSWORD_LENGTH_ERROR);
+                    }
+                    if (arg == LONG_MIN) {
+                        fprintf(stderr, "Password length is below the minimum of %d\n",
+                                MIN_PASSWORD_LENGTH);
+                        exit(STATUS_PASSWORD_LENGTH_ERROR);
+                    }
+                }
 
-        if (validate_length(arg) != STATUS_OK) {
-          fprintf(stderr, "Length must be between %d and %d\n", MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH);
-          exit(STATUS_PASSWORD_LENGTH_ERROR);
-        }
-        
-        char *password = malloc(arg + 1);
-        if(!password) {
-          perror("malloc");
-          exit(STATUS_ALLOCATION_ERROR);
-        }
+                if (*endptr != '\0') {
+                    fprintf(stderr, "Trailing characters after the number\n");
+                    exit(STATUS_NOT_A_NUMBER);
+                }
 
-        if (generate_password(password, arg) != STATUS_OK) {
-          explicit_bzero(password, arg + 1);
-          free(password);
-          exit(STATUS_GENERATE_PASSWORD_ERROR);
-        }
+                if (optarg == endptr) {
+                    fprintf(stderr, "Not a valid number\n");
+                    exit(STATUS_NOT_A_NUMBER);
+                }
 
-        print_password(password);
-        if (copy_to_clipboard(display_server, password) != STATUS_OK) {
-          fprintf(stderr, "warning: could not copy password to clipboard\n");
-        }
+                if (validate_length(arg) != STATUS_OK) {
+                    fprintf(stderr, "Length must be between %d and %d\n", MIN_PASSWORD_LENGTH,
+                            MAX_PASSWORD_LENGTH);
+                    exit(STATUS_PASSWORD_LENGTH_ERROR);
+                }
 
-        explicit_bzero(password, arg + 1);
-        free(password);
-        
-        return STATUS_OK;
-      }
-      case 'h':
-        printf("Use -g <len> for password generation\n");
-        return STATUS_OK;
-      case '?':
-        if (optopt == 'g') {
-          fprintf(stderr, "-g requires a number argument\n");
-        } else if (optopt != 0) {
-          fprintf(stderr, "Unknown option: -%c\n", optopt);
-        } else {
-          fprintf(stderr, "Unknown option\n");
+                char *password = malloc(arg + 1);
+                if (!password) {
+                    perror("malloc");
+                    exit(STATUS_ALLOCATION_ERROR);
+                }
+
+                if (generate_password(password, arg) != STATUS_OK) {
+                    explicit_bzero(password, arg + 1);
+                    free(password);
+                    exit(STATUS_GENERATE_PASSWORD_ERROR);
+                }
+
+                print_password(password);
+                if (copy_to_clipboard(display_server, password) != STATUS_OK) {
+                    fprintf(stderr, "warning: could not copy password to clipboard\n");
+                }
+
+                explicit_bzero(password, arg + 1);
+                free(password);
+
+                return STATUS_OK;
+            }
+            case 'h':
+                printf("Use -g <len> for password generation\n");
+                return STATUS_OK;
+            case '?':
+                if (optopt == 'g') {
+                    fprintf(stderr, "-g requires a number argument\n");
+                } else if (optopt != 0) {
+                    fprintf(stderr, "Unknown option: -%c\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option\n");
+                }
+                exit(STATUS_UNKNOWN_OPTIONS);
         }
+    }
+    if (optind < argc) {
+        fprintf(stderr, "Unknown argument: %s. Use -g <len> or -h\n", argv[optind]);
         exit(STATUS_UNKNOWN_OPTIONS);
     }
-  }
-  if (optind < argc) {
-    fprintf(stderr, "Unknown argument: %s. Use -g <len> or -h\n", argv[optind]);
-    exit(STATUS_UNKNOWN_OPTIONS);
-  }
-  return STATUS_OK;
+    return STATUS_OK;
 }
